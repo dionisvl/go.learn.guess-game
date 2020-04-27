1377a51170bbe4b22056bfdb1e592040d4ec17b2
den <dden_vlad@mail.ru>
2020-04-27 20:01:58 +0300

    init

---
+++ guess.go
@@ -0,0 +1,51 @@
+//guess игра, в которой игрок должен угадать случайное число
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix() //Получаем текущую дату и время в виде числа.
+	rand.Seed(seconds)           //инициализируем генератор случайных чисел
+	target := rand.Intn(100) + 1 //Генерируем целое число от 1 до 100
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+	reader := bufio.NewReader(os.Stdin) //Для чтения с клавиатуры
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good Job! You guessed it!")
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was: ", target)
+	}
+}
